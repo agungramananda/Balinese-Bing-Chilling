0d0142d61a4594a43a46043e0e346dcbba6ee5dd
Agung Ramananda <agungramananda@gmail.com>
2024-06-10 14:02:14 +0800

    Update catalog.blade.php to display ice cream flavors with proper formatting

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -77,8 +77,9 @@
                 <h3>Type : {{ $iceCream->type->type }}</h3>
                 <div class='flavor'>
                   <h3>Flavor :</h3>
+                  <ul>
                     @foreach($iceCream->flavor as $flavor)
-                      <span>{{ $flavor->flavor }}</span>
+                      <h4>{{ $flavor->flavor }}</h4>
                     @endforeach
                 </div>
                 <h3>Size : {{ $iceCream->size->size }}</h3>
